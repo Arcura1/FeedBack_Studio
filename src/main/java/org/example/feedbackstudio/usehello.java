ef6c775e7420cccd791c5be1317274e13036da53
Oray isik <87896994+Arcura1@users.noreply.github.com>
2024-10-01 00:34:33 +0300

    ORAY TEST

--- src/main/java/org/example/feedbackstudio/usehello.java
+++ src/main/java/org/example/feedbackstudio/usehello.java
@@ -22,7 +22,7 @@ public class usehello {
     }
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World!tunaacar";
+        return "Hello, World!tunaacaroray";
     }
 
 }
