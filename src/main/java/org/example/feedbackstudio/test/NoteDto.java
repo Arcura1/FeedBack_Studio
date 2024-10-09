e094b5d98a8d336a8d13091a7d1ae4fadc208d5c
Oray isik <87896994+Arcura1@users.noreply.github.com>
2024-10-09 20:08:14 +0300

    develop

--- src/main/java/org/example/feedbackstudio/test/NoteDto.java
+++ src/main/java/org/example/feedbackstudio/test/NoteDto.java
@@ -1,9 +1,5 @@
 package org.example.feedbackstudio.test;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
 
@@ -17,32 +13,41 @@ public class NoteDto {
     public NoteDto() {
 
     }
+
     public NoteDto(String id, int Xcoordinate, int Ycoordinate) {
         this.id = id;
         this.Xcoordinate = Xcoordinate;
         this.Ycoordinate = Ycoordinate;
     }
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public int getXcoordinate() {
         return Xcoordinate;
     }
+
     public void setXcoordinate(int Xcoordinate) {
         this.Xcoordinate = Xcoordinate;
     }
+
     public int getYcoordinate() {
         return Ycoordinate;
     }
+
     public void setYcoordinate(int Ycoordinate) {
         this.Ycoordinate = Ycoordinate;
     }
+
     public String getNote() {
         return note;
     }
+
     public void setNote(String note) {
         this.note = note;
     }
