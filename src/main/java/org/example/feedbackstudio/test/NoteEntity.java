4e4cbf99666b8213f0489ba873a9c9463d14fbc6
Oray isik <87896994+Arcura1@users.noreply.github.com>
2024-10-09 20:24:17 +0300

    develop

--- src/main/java/org/example/feedbackstudio/test/NoteEntity.java
+++ src/main/java/org/example/feedbackstudio/test/NoteEntity.java
@@ -15,30 +15,39 @@ public class NoteEntity {
     public NoteEntity() {
 
     }
+
     public NoteEntity(int Xcoordinate, int Ycoordinate) {
         this.Xcoordinate = Xcoordinate;
     }
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public int getXcoordinate() {
         return Xcoordinate;
     }
+
     public void setXcoordinate(int Xcoordinate) {
         this.Xcoordinate = Xcoordinate;
     }
+
     public int getYcoordinate() {
         return Ycoordinate;
     }
+
     public void setYcoordinate(int Ycoordinate) {
         this.Ycoordinate = Ycoordinate;
     }
+
     public String getNote() {
         return note;
     }
+
     public void setNote(String note) {
         this.note = note;
     }
