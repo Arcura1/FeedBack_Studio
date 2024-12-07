7de45e9df832a9dbdcd8b52c4274e26248db7501
Red <95549175+TheRedSS@users.noreply.github.com>
2024-12-07 19:36:59 +0300

    berke test

--- src/main/java/org/example/feedbackstudio/note/RestController/HomeworkController.java
+++ src/main/java/org/example/feedbackstudio/note/RestController/HomeworkController.java
@@ -1,11 +1,48 @@
 package org.example.feedbackstudio.note.RestController;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.feedbackstudio.note.entity.HomeworkEntity;
+import org.example.feedbackstudio.note.repository.HomeworkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/homeworks")
 @CrossOrigin(origins = "*")
 public class HomeworkController {
 
+    @Autowired
+    private HomeworkRepository repository;
+
+    @GetMapping
+    public List<HomeworkEntity> getAllHomeworks() {
+        return (List<HomeworkEntity>) repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public HomeworkEntity getHomeworkById(@PathVariable String id) {
+        return repository.findById(Integer.valueOf(id)).orElse(null);
+    }
+
+    @PostMapping
+    public HomeworkEntity createHomework(@RequestBody HomeworkEntity homework) {
+        return repository.save(homework);
+    }
+
+    @PutMapping("/{id}")
+    public HomeworkEntity updateHomework(@PathVariable String id, @RequestBody HomeworkEntity updatedHomework) {
+        HomeworkEntity existingHomework = repository.findById(Integer.valueOf(id)).orElse(null);
+        if (existingHomework != null) {
+            existingHomework.setTitle(updatedHomework.getTitle());
+            existingHomework.setDescription(updatedHomework.getDescription());
+            return repository.save(existingHomework);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteHomework(@PathVariable String id) {
+        repository.deleteById(Integer.valueOf(id));
+    }
 }
--- src/main/java/org/example/feedbackstudio/note/repository/HomeworkRepository.java
+++ src/main/java/org/example/feedbackstudio/note/repository/HomeworkRepository.java
@@ -4,4 +4,6 @@ import org.example.feedbackstudio.note.entity.HomeworkEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface HomeworkRepository extends CrudRepository<HomeworkEntity, Integer> {
+
 }
+
