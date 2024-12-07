274b107e782e33d0ab847e4b3c9c673b1f08150b
acart <acrtunahan@outlook.com>
2024-12-07 18:13:15 +0300

    n

--- src/main/java/org/example/feedbackstudio/login/entity/User.java
+++ src/main/java/org/example/feedbackstudio/login/entity/User.java
@@ -2,12 +2,13 @@ package org.example.feedbackstudio.login.entity;
 
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
 @Getter
 @Setter
+@NoArgsConstructor
 @Document(collection = "users")
 public class User {
 
@@ -20,18 +21,61 @@ public class User {
     private String password;
     private String role;
 
-    // Constructor
-    public User() {
-        this.role = "student";
-    }
-
-    public User(String firstName, String lastName, String email, String phone, String password) {
+    public User(String firstName, String lastName, String email, String phone, String password, String role) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
         this.password = password;
-        this.role = "student";
+        this.role = role != null ? role : "student";
     }
 
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
