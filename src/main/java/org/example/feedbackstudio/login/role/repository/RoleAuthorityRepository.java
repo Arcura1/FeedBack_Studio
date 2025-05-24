e8da15ffc586ae8d02e4f92900c7c7e8ef226c67
Oray isik <87896994+Arcura1@users.noreply.github.com>
2025-05-24 17:48:10 +0300

    some changes

--- src/main/java/org/example/feedbackstudio/login/role/repository/RoleAuthorityRepository.java
+++ src/main/java/org/example/feedbackstudio/login/role/repository/RoleAuthorityRepository.java
@@ -1,7 +1,9 @@
 package org.example.feedbackstudio.login.role.repository;
 
 import org.example.feedbackstudio.login.role.entity.roleAuthorityEntitiy;
+import org.example.feedbackstudio.login.role.entity.roleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RoleAuthorityRepository extends JpaRepository<roleAuthorityEntitiy, Long> {
+public interface RoleAuthorityRepository extends JpaRepository<roleAuthorityEntitiy, Long>, JpaSpecificationExecutor<roleEntity> {
 }
